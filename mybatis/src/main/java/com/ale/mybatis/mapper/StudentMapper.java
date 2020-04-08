719e5c25971e39b96f539448041ea4758591da97
alewu <wywujiawei@163.com>
2020-04-08 12:49:35 +0800

    update

---
+++ mybatis/src/main/java/com/ale/mybatis/mapper/StudentMapper.java
@@ -0,0 +1,7 @@
+package com.ale.mybatis.mapper;
+
+import com.ale.mybatis.entity.Student;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface StudentMapper extends BaseMapper<Student> {
+}
