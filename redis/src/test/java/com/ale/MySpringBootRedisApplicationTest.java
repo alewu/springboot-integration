fdba607d88c43a0fde086cffda3f1fd5e30624ca
alewu <wywujiawei@163.com>
2020-04-08 12:49:35 +0800

    update

---
+++ redis/src/test/java/com/ale/MySpringBootRedisApplicationTest.java
@@ -0,0 +1,39 @@
+package com.ale;
+
+import com.ale.entity.UserEntity;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.Serializable;
+
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class MySpringBootRedisApplicationTest {
+    @Autowired
+    private RedisTemplate<String, String> strRedisTemplate;
+    @Autowired
+    private RedisTemplate<String, Object> serializableRedisTemplate;
+
+    @Test
+    public void testString() {
+        strRedisTemplate.opsForValue().set("strKey", "zwqh");
+        System.out.println(strRedisTemplate.opsForValue().get("strKey"));
+    }
+
+    @Test
+    public void testSerializable() {
+        UserEntity user=new UserEntity();
+        user.setId(1L);
+        user.setUserName("朝雾轻寒");
+        user.setUserSex("男");
+        serializableRedisTemplate.opsForValue().set("user", user);
+        UserEntity user2 = (UserEntity) serializableRedisTemplate.opsForValue().get("user");
+        System.out.println("user:"+user2.getId()+","+user2.getUserName()+","+user2.getUserSex());
+    }
+
+}
