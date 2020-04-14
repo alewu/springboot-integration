1236edcab8aec2d92c7b1e5b5156dd21c112700c
alewu <wywujiawei@163.com>
2020-04-14 09:43:26 +0800

    add mq

---
+++ mq/rabbitmq/src/test/java/com/ale/rabbitmq/pubsub/ReceiveLogs.java
@@ -0,0 +1,38 @@
+package com.ale.rabbitmq.pubsub;
+
+import com.alewu.util.ConnectionUtil;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author alewu
+ * @date 2018/7/8 19:22
+ */
+public class ReceiveLogs {
+    private static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] argv) throws Exception {
+        Connection connection = ConnectionUtil.getConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+
+        String queueName = channel.queueDeclare().getQueue();
+        // 绑定队列到交换机
+        channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, StandardCharsets.UTF_8);
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        channel.basicConsume(queueName, true, consumer);
+    }
+}
---
+++ mq/rabbitmq/src/test/java/com/ale/rabbitmq/simple/Receive.java
@@ -0,0 +1,38 @@
+package com.ale.rabbitmq.simple;
+
+import com.alewu.util.ConnectionUtil;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author alewu
+ * @date 2018/7/5 14:23
+ */
+public class Receive {
+    private final static String QUEUE_NAME = "test_queue";
+
+    public static void main(String[] argv) throws Exception {
+
+        // 获取到连接以及mq通道
+        Connection connection = ConnectionUtil.getConnection();
+        Channel channel = connection.createChannel();
+
+        // 声明队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, StandardCharsets.UTF_8);
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+}
