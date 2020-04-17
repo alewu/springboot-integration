1a018a497471fb366a7e7adeeb0bb739cf9b730d
alewu <wywujiawei@163.com>
2020-04-17 17:53:49 +0800

    spring unit test

---
+++ mvc/src/main/java/com/ale/MySpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.ale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ale
+ */
+@SpringBootApplication
+public class MySpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringbootApplication.class, args);
+    }
+}
---
+++ mvc/src/main/java/com/ale/entity/User.java
@@ -0,0 +1,14 @@
+package com.ale.entity;
+
+import lombok.Data;
+
+/**
+ * @author ale
+ */
+@Data
+public class User {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String email;
+}
