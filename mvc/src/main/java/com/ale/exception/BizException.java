09652433a199ab748e775bde3db63c01659d8d60
alewu <wywujiawei@163.com>
2020-04-20 19:28:23 +0800

    spring mybatis plus

---
+++ mvc/src/main/java/com/ale/exception/BizException.java
@@ -0,0 +1,26 @@
+package com.ale.exception;
+
+import lombok.Data;
+
+/**
+ * 业务异常跳转。
+ *
+ * @author ale
+ */
+@Data
+public class BizException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    private CodeEnum code;
+    private String remark;
+
+    public BizException(CodeEnum code) {
+        super(code.getMessage());
+        this.code = code;
+    }
+
+    public BizException withRemark(String remark) {
+        this.remark = remark;
+        return this;
+    }
+}
