23322d80209892a3d4672b389e7314bb7897575f
alewu <wywujiawei@163.com>
2020-04-23 19:10:00 +0800

    spring mybatis plus

---
+++ mvc/src/main/java/com/ale/mapper/DeptMapper.java
@@ -0,0 +1,10 @@
+package com.ale.mapper;
+
+import com.ale.entity.Dept;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author ale
+ */
+public interface DeptMapper extends BaseMapper<Dept> {
+}
