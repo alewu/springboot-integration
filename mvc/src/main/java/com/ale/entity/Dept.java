337e3a2e980b77b2456c7c21e8fc370343ddef68
alewu <wywujiawei@163.com>
2020-04-24 11:08:04 +0800

    split mybatis

--- mvc/src/main/java/com/ale/entity/Dept.java
+++
@@ -1,9 +0,0 @@
-package com.ale.entity;
-
-import lombok.Data;
-
-@Data
-public class Dept {
-    private Integer id;
-    private String deptName;
-}
---
+++ mybatis/mybatis-raw/src/main/java/com/ale/entity/Student.java
@@ -0,0 +1,11 @@
+package com.ale.entity;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private Integer id;
+    private String stuName;
+    private Integer sex;
+    private Integer age;
+}
