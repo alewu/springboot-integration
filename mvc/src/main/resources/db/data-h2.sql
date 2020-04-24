6a86ce6672ceeafea640d6cd97bd77ae5c2c935c
alewu <wywujiawei@163.com>
2020-04-24 09:48:08 +0800

    spring mybatis plus

--- mvc/src/main/resources/db/data-h2.sql
+++ mvc/src/main/resources/db/data-h2.sql
@@ -1,14 +1,18 @@
 DELETE FROM user;
 
 INSERT INTO user (id, name, age, email) VALUES
-(1, 'neo', 18, 'smile1@ityouknow.com'),
-(2, 'keep', 36, 'smile@ityouknow.com'),
-(3, 'pure', 28, 'smile@ityouknow.com'),
-(4, 'smile', 21, 'smile@ityouknow.com'),
-(5, 'it', 24, 'smile@ityouknow.com');
+(1, 'Jack', 18, 'jack@ityouknow.com'),
+(2, 'Rose', 36, 'rose@ityouknow.com'),
+(3, 'Bob', 28, 'bob@ityouknow.com'),
+(4, 'Smile', 21, 'smile@ityouknow.com'),
+(5, 'Alice', 24, 'alice@ityouknow.com');
 
 DELETE FROM dept;
 
 INSERT INTO dept (id, dept_name) VALUES
-(1, 'it'),
-(2, 'hr');
+(1, 'marketing'),
+(2, 'sales'),
+(3, 'purchasing'),
+(4, 'it'),
+(5, 'hr'),
+(6, 'engineering');
