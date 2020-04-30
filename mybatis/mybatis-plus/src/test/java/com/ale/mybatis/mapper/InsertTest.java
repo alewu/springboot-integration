77d1ec8de41efde59680a5d237ef34ea1aba6ad7
alewu <wywujiawei@163.com>
2020-04-30 18:20:57 +0800

    mybatis update

--- mybatis/mybatis-plus/src/test/java/com/ale/mybatis/mapper/InsertTest.java
+++ mybatis/mybatis-plus/src/test/java/com/ale/mybatis/mapper/InsertTest.java
@@ -2,9 +2,12 @@ package com.ale.mybatis.mapper;
 
 
 import com.ale.mybatis.entity.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class InsertTest {
@@ -19,5 +22,16 @@ public class InsertTest {
         user.setEmail("123@gmail");
         user.setName("test");
         userMapper.insert(user);
+        assertEquals(55, user.getId());
+    }
+
+    @Test
+    public void testSaveOrInsert(){
+        User user = new User();
+        user.setId(55);
+        user.setAge(12);
+        user.setEmail("123@gmail");
+        user.setName("test");
+
     }
 }
---
+++ mybatis/mybatis-raw/src/test/java/com/ale/mapper/StudentMapperTest.java
@@ -0,0 +1,38 @@
+package com.ale.mapper;
+
+import com.ale.entity.Student;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class StudentMapperTest {
+    @Autowired
+    private StudentMapper studentMapper;
+
+    @Test
+    public void testSelect() {
+        List<Student> students = Lists.newArrayList();
+        Student student = new Student();
+        student.setName("java1");
+        student.setAge(12);
+        student.setSex(11);
+        Student student1 = new Student();
+        student1.setName("python");
+        student1.setAge(12);
+        student1.setSex(1);
+        Student student2 = new Student();
+        student2.setName("c");
+        student2.setAge(12);
+        student2.setSex(1);
+        students.add(student);
+        students.add(student1);
+        students.add(student2);
+        studentMapper.addOrUpdateBatch(students);
+
+    }
+
+}
