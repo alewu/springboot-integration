744e6a0531278ce06bdd13aa38fcc79ba1bb86e7
alewu <wywujiawei@163.com>
2020-05-07 18:57:55 +0800

    add spring event

--- mvc/src/main/resources/db/data-h2.sql
+++ mvc/src/main/resources/db/data-h2.sql
@@ -1,18 +1,6 @@
-DELETE FROM user;
+DELETE FROM tb_order;
 
-INSERT INTO user (id, name, age, email) VALUES
-(1, 'Jack', 18, 'jack@ityouknow.com'),
-(2, 'Rose', 36, 'rose@ityouknow.com'),
-(3, 'Bob', 28, 'bob@ityouknow.com'),
-(4, 'Smile', 21, 'smile@ityouknow.com'),
-(5, 'Alice', 24, 'alice@ityouknow.com');
+INSERT INTO tb_order (id, user_name, goods_name) VALUES
+(1, 'Jack',  'jack@ityouknow.com'),
+(2, 'Rose',  'rose@ityouknow.com')
 
-DELETE FROM dept;
-
-INSERT INTO dept (id, dept_name) VALUES
-(1, 'marketing'),
-(2, 'sales'),
-(3, 'purchasing'),
-(4, 'it'),
-(5, 'hr'),
-(6, 'engineering');
