b440974069703869a1742147db19b799f92d55d0
alewu <wywujiawei@163.com>
2020-05-07 18:57:55 +0800

    add spring event

---
+++ mvc/src/main/java/com/ale/mapper/OrderMapper.java
@@ -0,0 +1,10 @@
+package com.ale.mapper;
+
+import com.ale.entity.Order;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author alewu
+ */
+public interface OrderMapper extends BaseMapper<Order> {
+}
