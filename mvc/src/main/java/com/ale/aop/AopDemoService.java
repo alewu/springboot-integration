a37ff34af2088c0703ff3ac79c43fd2e0a46ee3f
alewu <wywujiawei@163.com>
2020-06-19 17:25:40 +0800

    update

---
+++ mvc/src/main/java/com/ale/aop/AopDemoService.java
@@ -0,0 +1,18 @@
+package com.ale.aop;
+
+/**
+ * The interface Aop demo service.
+ *
+ * @author alewu
+ * @date 2020 /6/19
+ */
+public interface AopDemoService {
+
+    /**
+     * Open base door.
+     *
+     * @param door the door
+     * @return the base door
+     */
+    BaseDoor open(BaseDoor door);
+}
