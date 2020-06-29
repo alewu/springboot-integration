720e12a7a36bd936625d95eef72599e87898ead5
alewu <wywujiawei@163.com>
2020-06-29 10:15:14 +0800

    update

--- mvc/src/main/java/com/ale/exception/GlobalExceptionHandler.java
+++ mvc/src/main/java/com/ale/exception/GlobalExceptionHandler.java
@@ -1,23 +1,25 @@
 package com.ale.exception;
 
+import com.ale.common.BusinessExceptionEnum;
 import com.ale.common.ResponseResult;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.xml.bind.ValidationException;
-import java.util.StringJoiner;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.lang.reflect.Field;
+import java.util.Set;
 
 /**
  * The type Global exception handler.
  *
- * @author ale  规范：流程跳转尽量避免使用抛 BizException 来做控制。
+ * @author ale 规范：流程跳转尽量避免使用抛 BizException 来做控制。
  */
 @RestControllerAdvice
 @Slf4j
@@ -26,7 +28,7 @@ public class GlobalExceptionHandler {
      * 处理未捕获的Exception
      *
      * @param e 异常
-     * @return 统一响应体  data:null
+     * @return 统一响应体 data:null
      */
     @ExceptionHandler(Exception.class)
     public ResponseResult handleException(Exception e) {
@@ -39,7 +41,7 @@ public class GlobalExceptionHandler {
      * 处理未捕获的RuntimeException
      *
      * @param e 运行异常
-     * @return 统一响应体  data:null
+     * @return 统一响应体 data:null
      */
     @ExceptionHandler(RuntimeException.class)
     public ResponseResult handleRuntimeException(RuntimeException e) {
@@ -52,7 +54,7 @@ public class GlobalExceptionHandler {
      * 处理业务异常BaseException
      *
      * @param e 业务异常
-     * @return 统一响应体  data:null
+     * @return 统一响应体 data:null
      */
     @ExceptionHandler(BaseException.class)
     public ResponseResult handleBaseException(BaseException e) {
@@ -64,25 +66,45 @@ public class GlobalExceptionHandler {
     /**
      * 参数校验错误
      *
-     * @param ex the ex
+     * @param e the e
      * @return response result
      */
-    @ExceptionHandler(ValidationException.class)
-    public ResponseResult handleValidationException(ValidationException ex) {
-        return new ResponseResult(ResponseCodeEnum.RESOURCES_NOT_EXIST.getCode(), ex.getCause().getMessage(), null);
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseResult<String> handleConstraintViolationException(ConstraintViolationException e) {
+        Set<ConstraintViolation<?>> constraintViolations = e.getConstraintViolations();
+        String messageTemplate = constraintViolations.iterator().next().getMessageTemplate();
+        log.warn(messageTemplate);
+        return ResponseResult.failed(BusinessExceptionEnum.FILE_UPLOAD_EXCEPTION);
     }
 
     /**
      * 字段校验不通过异常
      *
-     * @param ex the ex
+     * @param e the e
      * @return response result
+     * @throws NoSuchFieldException the no such field exception
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseResult handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        StringJoiner sj = new StringJoiner(";");
-        ex.getBindingResult().getFieldErrors().forEach(x -> sj.add(x.getDefaultMessage()));
-        return new ResponseResult(ResponseCodeEnum.SERVICE_ERROR.getCode(), sj.toString(), null);
+    public ResponseResult<String> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) throws NoSuchFieldException {
+        // 从异常对象中拿到错误信息
+        String defaultMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+
+        // 参数的Class对象，等下好通过字段名称获取Field对象
+        Class<?> parameterType = e.getParameter().getParameterType();
+        // 拿到错误的字段名称
+        String fieldName = e.getBindingResult().getFieldError().getField();
+        Field field = parameterType.getDeclaredField(fieldName);
+        // 获取Field对象上的自定义注解
+        //        ExceptionCode annotation = field.getAnnotation(ExceptionCode.class);
+        //
+        //        // 有注解的话就返回注解的响应信息
+        //        if (annotation != null) {
+        //            return new ResponseResult<>(annotation.value(),annotation.message(),defaultMessage);
+        //        }
+
+        // 没有注解就提取错误提示信息进行返回统一错误码
+        return ResponseResult.failed(BusinessExceptionEnum.RESOURCE_NOT_FOUND);
     }
 
     /**
