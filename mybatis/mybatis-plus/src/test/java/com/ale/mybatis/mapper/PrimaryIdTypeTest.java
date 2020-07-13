8b3407f3ce975d4179ec5616d902ffeeee69bef8
alewu <wywujiawei@163.com>
2020-07-13 18:37:02 +0800

    update

---
+++ mybatis/mybatis-plus/src/test/java/com/ale/mybatis/mapper/PrimaryIdTypeTest.java
@@ -0,0 +1,12 @@
+package com.ale.mybatis.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimaryIdTypeTest {
+    @Test
+    void test() {
+
+    }
+}
