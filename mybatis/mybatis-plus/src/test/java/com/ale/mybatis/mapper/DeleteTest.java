6dc03cae9ef089f75f6a7a1ad5f80b21463022c0
alewu <wywujiawei@163.com>
2020-07-13 18:18:52 +0800

    update

--- mybatis/mybatis-plus/src/test/java/com/ale/mybatis/mapper/DeleteTest.java
+++ mybatis/mybatis-plus/src/test/java/com/ale/mybatis/mapper/DeleteTest.java
@@ -1,7 +1,9 @@
 package com.ale.mybatis.mapper;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,14 +12,16 @@ import java.util.Map;
  * @author alewu
  * @date 2020/7/13
  */
+@SpringBootTest
 class DeleteTest {
     @Autowired
-    private DeptMapper deptMapper;
+    private UserMapper userMapper;
 
     @Test
     void testDeleteByMap() {
         Map<String, Object> map = new HashMap<>();
-        map.put("dept_name", "it");
-        deptMapper.deleteByMap(map);
+        map.put("age", 12);
+        int i = userMapper.deleteByMap(map);
+        Assertions.assertTrue(i > 0);
     }
 }
