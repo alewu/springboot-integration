34361408e91d3ab066e2fe6914c9d4a95fd49b1e
alewu <wywujiawei@163.com>
2020-07-14 19:55:57 +0800

    update

--- mq/rabbitmq/src/main/java/com/ale/rabbitmq/delayqueue/XdelayConfig.java
+++ mq/rabbitmq/src/main/java/com/ale/rabbitmq/delayqueue/XdelayConfig.java
@@ -1,15 +1,9 @@
 package com.ale.rabbitmq.delayqueue;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.CustomExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * The type Xdelay config.
@@ -34,38 +28,14 @@ public class XdelayConfig {
      */
     public static final String DELAY_QUEUE = "delay_queue";
 
-    /**
-     * 延时队列交换机
-     * 注意这里的交换机类型：CustomExchange
-     *
-     * @return custom exchange
-     */
-    @Bean
-    public CustomExchange delayExchange() {
-        Map<String, Object> args = new HashMap<>();
-        args.put("x-delayed-type", "direct");
-        return new CustomExchange(DELAY_EXCHANGE, "x-delayed-message", true, false, args);
-    }
 
-    /**
-     * 延时队列
-     *
-     * @return queue queue
-     */
     @Bean
-    public Queue delayQueue() {
-        return new Queue(DELAY_QUEUE, true);
-    }
-
-    /**
-     * 给延时队列绑定交换机
-     *
-     * @param cfgDelayQueue        the cfg delay queue
-     * @param cfgUserDelayExchange the cfg user delay exchange
-     * @return binding binding
-     */
-    @Bean
-    public Binding cfgDelayBinding(Queue cfgDelayQueue, CustomExchange cfgUserDelayExchange) {
-        return BindingBuilder.bind(cfgDelayQueue).to(cfgUserDelayExchange).with(DELAY_KEY).noargs();
+    public Declarables delayBindings() {
+        Queue delayQueue = QueueBuilder.durable(DELAY_QUEUE).build();
+        DirectExchange delayExchange = ExchangeBuilder.directExchange(DELAY_EXCHANGE).delayed().durable(true).build();
+        return new Declarables(
+                delayQueue,
+                delayExchange,
+                BindingBuilder.bind(delayQueue).to(delayExchange).withQueueName());
     }
 }
--- mybatis/mybatis-plus/src/main/java/com/ale/mybatis/config/MybatisPlusConfig.java
+++ mybatis/mybatis-plus/src/main/java/com/ale/mybatis/config/MybatisPlusConfig.java
@@ -5,11 +5,10 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParser
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @Configuration
-@EnableTransactionManagement
+// @EnableTransactionManagement
 @MapperScan("com.ale.mybatis.mapper")
 public class MybatisPlusConfig {
     @Bean
