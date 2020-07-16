9296e4c5479c47e5b12e7d48d645f2aa6381661e
alewu <wywujiawei@163.com>
2020-07-16 12:31:35 +0800

    update

---
+++ redis/src/main/java/com/ale/client/RedisClientApplication.java
@@ -0,0 +1,15 @@
+package com.ale.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author alewu
+ * @date 2020/7/16
+ */
+@SpringBootApplication
+public class RedisClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisClientApplication.class, args);
+    }
+}
---
+++ redis/src/main/java/com/ale/client/RedisConfig.java
@@ -0,0 +1,15 @@
+package com.ale.client;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author alewu
+ */
+@Configuration
+@EnableAutoConfiguration
+public class RedisConfig {
+
+
+}
