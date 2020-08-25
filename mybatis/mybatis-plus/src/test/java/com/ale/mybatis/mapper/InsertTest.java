e3c21e2a5aa5c0b7118329e99a92edc91e58ae2d
alewu <wywujiawei@163.com>
2020-08-25 10:02:16 +0800

    update

--- mybatis/mybatis-plus/src/test/java/com/ale/mybatis/mapper/InsertTest.java
+++ mybatis/mybatis-plus/src/test/java/com/ale/mybatis/mapper/InsertTest.java
@@ -4,32 +4,51 @@ package com.ale.mybatis.mapper;
 import com.ale.mybatis.entity.User;
 import com.ale.mybatis.service.UserService;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.Instant;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@Slf4j
 class InsertTest {
     @Autowired
     private UserMapper userMapper;
     @Autowired
     private UserService userService;
+    long start = 0l;
+
+    @BeforeEach
+    void before() {
+        start = Instant.now().toEpochMilli();
+    }
+
+    @AfterEach
+    void after() {
+        long end = Instant.now().toEpochMilli();
+        log.info("耗时{} ms", end - start);
+    }
 
     @Test
     void testInsert() {
-        User user = new User();
-        user.setId(55);
-        user.setAge(12);
-        user.setEmail("123@gmail");
-        user.setName("test");
-        userMapper.insert(user);
-        assertEquals(55, user.getId());
+        for (int i = 0; i < 100; i++) {
+            User user = new User();
+            user.setId(55);
+            user.setAge(12);
+            user.setEmail("123@gmail");
+            user.setName("test");
+            userMapper.insert(user);
+        }
+
+        //        assertEquals(55, user.getId());
     }
 
     @Test
