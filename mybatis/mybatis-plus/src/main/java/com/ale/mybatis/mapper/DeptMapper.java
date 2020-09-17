508ce4f9d32dd22b372375f84f466d962c49d2e8
alewu <wywujiawei@163.com>
2020-09-17 23:10:20 +0800

    update

--- mybatis/mybatis-plus/src/main/java/com/ale/mybatis/mapper/DeptMapper.java
+++ mybatis/mybatis-plus/src/main/java/com/ale/mybatis/mapper/DeptMapper.java
@@ -3,9 +3,11 @@ package com.ale.mybatis.mapper;
 
 import com.ale.mybatis.entity.Dept;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author ale
  */
+@Repository
 public interface DeptMapper extends BaseMapper<Dept> {
 }
--- mybatis/mybatis-plus/src/main/java/com/ale/mybatis/mapper/UserMapper.java
+++ mybatis/mybatis-plus/src/main/java/com/ale/mybatis/mapper/UserMapper.java
@@ -3,9 +3,11 @@ package com.ale.mybatis.mapper;
 
 import com.ale.mybatis.entity.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author ale
  */
+@Repository
 public interface UserMapper extends BaseMapper<User> {
 }
