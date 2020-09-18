c69b5415eb834b1882c5aaf91697e6f211b72906
alewu <wywujiawei@163.com>
2020-09-18 10:44:13 +0800

    update

--- mybatis/mybatis-raw/src/main/resources/db/data-mysql.sql
+++ mybatis/mybatis-raw/src/main/resources/db/data-mysql.sql
@@ -1,9 +1,9 @@
 DELETE
 FROM student;
 
-INSERT INTO student (id, student_name, gender, age, email)
-VALUES (1, 'Jack', 1, 12, 'jack@gmail.com'),
-       (2, 'Rose', 0, 13, 'rose@gmail.com'),
-       (3, 'Bob', 1, 34, 'bob@gmail.com'),
-       (4, 'Smile', 0, 28, 'smile@gmail.com'),
-       (5, 'Alice', 0, 75, 'alice@gmail.com');
+INSERT INTO student (id, class_id, student_name, gender, age, email)
+VALUES (1, 1, 'Jack', 1, 12, 'jack@gmail.com'),
+       (2, 1, 'Rose', 0, 13, 'rose@gmail.com'),
+       (3, 1, 'Bob', 1, 34, 'bob@gmail.com'),
+       (4, 1, 'Smile', 0, 28, 'smile@gmail.com'),
+       (5, 1, 'Alice', 0, 75, 'alice@gmail.com');
--- mybatis/mybatis-raw/src/main/resources/db/schema-mysql.sql
+++ mybatis/mybatis-raw/src/main/resources/db/schema-mysql.sql
@@ -1,11 +1,31 @@
 DROP TABLE IF EXISTS student;
 
-CREATE TABLE student
+create table student
 (
-    id           BIGINT(20)  NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-    student_name VARCHAR(30) NULL DEFAULT NULL COMMENT '姓名',
-    gender       INT(11)     NULL DEFAULT NULL COMMENT '性别',
-    age          INT(11)     NULL DEFAULT NULL COMMENT '年龄',
-    email        VARCHAR(30) NULL DEFAULT NULL COMMENT '邮件',
-    PRIMARY KEY (id)
+    id           bigint auto_increment comment '主键ID'
+        primary key,
+    class_id     int         not null default 0 comment '班级id',
+    student_name varchar(30) null     default '' comment '姓名',
+    gender       int         null     default 0 comment '性别',
+    age          int         null     default 0 comment '年龄',
+    email        varchar(30) null     default '' comment '邮件'
 );
+
+
+DROP TABLE IF EXISTS class;
+create table class
+(
+    id         int unsigned auto_increment
+        primary key,
+    class_name varchar(255) default '' null
+)
+    comment '班级表';
+
+DROP TABLE IF EXISTS course;
+create table course
+(
+    id          int auto_increment
+        primary key,
+    course_name varchar(20) default '' not null comment '课程名称'
+)
+    comment '课程表';
