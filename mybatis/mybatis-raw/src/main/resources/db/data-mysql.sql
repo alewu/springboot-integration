848f1b7d34ee4e87fb62777f9c6b25e5f31575ab
alewu <wywujiawei@163.com>
2020-09-18 10:20:20 +0800

    update

--- mybatis/mybatis-raw/src/main/resources/db/data-mysql.sql
+++ mybatis/mybatis-raw/src/main/resources/db/data-mysql.sql
@@ -1,9 +1,9 @@
 DELETE
 FROM student;
 
-INSERT INTO student (id, student_name, gender, email)
-VALUES (1, 'Jack', 18, 'jack@ityouknow.com'),
-       (2, 'Rose', 36, 'rose@ityouknow.com'),
-       (3, 'Bob', 28, 'bob@ityouknow.com'),
-       (4, 'Smile', 21, 'smile@ityouknow.com'),
-       (5, 'Alice', 24, 'alice@ityouknow.com');
+INSERT INTO student (id, student_name, gender, age, email)
+VALUES (1, 'Jack', 1, 12, 'jack@gmail.com'),
+       (2, 'Rose', 0, 13, 'rose@gmail.com'),
+       (3, 'Bob', 1, 34, 'bob@gmail.com'),
+       (4, 'Smile', 0, 28, 'smile@gmail.com'),
+       (5, 'Alice', 0, 75, 'alice@gmail.com');
--- mybatis/mybatis-raw/src/main/resources/db/schema-mysql.sql
+++ mybatis/mybatis-raw/src/main/resources/db/schema-mysql.sql
@@ -5,6 +5,7 @@ CREATE TABLE student
     id           BIGINT(20)  NOT NULL AUTO_INCREMENT COMMENT '主键ID',
     student_name VARCHAR(30) NULL DEFAULT NULL COMMENT '姓名',
     gender       INT(11)     NULL DEFAULT NULL COMMENT '性别',
+    age          INT(11)     NULL DEFAULT NULL COMMENT '年龄',
     email        VARCHAR(30) NULL DEFAULT NULL COMMENT '邮件',
     PRIMARY KEY (id)
 );
