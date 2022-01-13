9cbd905c380b901132b0eb2a6ad11e271e0e1b24
alewu <wywujw@163.com>
2022-01-13 10:32:39 +0800

    update

--- api-docs/src/main/java/com/ale/apidocs/swagger/entity/User.java
+++ api-docs/src/main/java/com/ale/apidocs/swagger/entity/User.java
@@ -13,19 +13,19 @@ import javax.validation.constraints.NotNull;
 @Data
 public class User {
 
-    @ApiModelProperty(value = "主键ID")
+    @ApiModelProperty(value = "主键ID", example = "120000000000")
     @NotNull(message = " cannot be null!")
     private Long userId;
 
-    @ApiModelProperty(value = "姓名")
+    @ApiModelProperty(value = "姓名", example = "jack")
     @NotBlank(message = "name cannot be null!")
     private String userName;
 
-    @ApiModelProperty(value = "年龄")
+    @ApiModelProperty(value = "年龄", example = "32")
     @Min(value = 0, message = "Age should not be less than 0")
     @Max(value = 100, message = "Age should not be greater than 100")
     private Integer age;
 
-    @ApiModelProperty(value = "邮箱")
+    @ApiModelProperty(value = "邮箱", example = "alewu@163.com")
     private String email;
 }
