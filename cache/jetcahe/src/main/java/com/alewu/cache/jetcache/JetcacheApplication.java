a8db347c68b038f485a9c85106c6d5c70802fa27
alewu <wywujiawei@163.com>
2023-10-12 08:13:04 +0800

    update

---
+++ cache/jetcahe/src/main/java/com/alewu/cache/jetcache/JetcacheApplication.java
@@ -0,0 +1,19 @@
+package com.alewu.cache.jetcache;
+
+import com.alicp.jetcache.anno.config.EnableMethodCache;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+/**
+ *
+ * @author wywuj
+ */
+@EnableMethodCache(basePackages = "com.xxx.mp")
+@EnableConfigurationProperties
+@SpringBootApplication
+public class JetcacheApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JetcacheApplication.class);
+    }
+}
